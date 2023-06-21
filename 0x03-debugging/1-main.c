afd8fc5add6ca1e2f84a9239fbd75315face5b57
Jkmaguvhe <jkmaguvhe@gmail.com>
2023-06-21 12:04:44 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,11 +9,8 @@ int main(void)
 {
 
 int i;
-
 printf("Infinite loop incoming :(\n");
-
 i = 0;
-
 /*while (i < 10)*/
 
 /*{*/
@@ -21,9 +18,6 @@ i = 0;
 /*putchar(i);*/
 
 /*}*/
-
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
-
 }
