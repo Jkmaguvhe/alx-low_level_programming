4d3953985f36f593cc08ed075bae5b1d8b96a6d3
Jkmaguvhe <jkmaguvhe@gmail.com>
2023-06-23 21:34:31 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,18 +5,15 @@
  * main - prints a Fizz Buzz program
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-
  	int x;
 
  	for (x = 1; x <= 100; x++)
  	{
  	if ((x % 3 == 0) && (x % 5 == 0))
  	{
- 	printf(" FizzBuzz");
+ 	printf("FizzBuzz");
  	}
  	else if (x % 3 == 0)
  	{
@@ -37,5 +34,4 @@ int main(void)
  	}
 	printf("\n");
 	return (0);
-
 }
